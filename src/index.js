e55e8e0f76b614676daaf4475d1e1dd84eea3391
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-09-04 14:58:44 -0400

    validad + stats / creacion de funcion mdLinks

--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
 #!/usr/bin/env node
 const mdLinks = require('./md-links.js');
+const process = require('process');
+const pathNode = require('path');
 const chalk = require('chalk');
 
 
 let path = process.argv[2]
+ // path.resolve para convertir en absoluta  
+path = pathNode.resolve(path);
+ // path.normalize para normalizar el path en caso se que hayan errores de semántica
+path = pathNode.normalize(path);
 
 let options = {
   stats: false,
   validate: false,
 }
 
-// process.argv.forEach(element =>{
-//  if( element == "--stats"){
-//    options.stats = true
-//  }
-// if(element == "--validate"){
-//   options.validate = true
-// }
-// })
+process.argv.forEach(element =>{
+ if( element == "--stats" || element == "-s" ){
+   options.stats = true
+ }
+if(element == "--validate" || element == "-v"){
+  options.validate = true
+}
+})
 
-// mdLinks.mdLinks(path,options).then(res=>{
-//   if(options.validate && options.stats){
-//     return console.log(chalk.magenta("Total Links: "+ res.total)+"\n"+chalk.green("Ok Links: "+res.ok)+"\n"+chalk.red("Broken Links: "+res.broken))
-//   }
-//   if(options.validate){
-//     if(res.length === 0){
-//       return console.log(chalk.red("No se encontraron links"))
-//     }
-//     let validateLinks = res.map(x=>x.file+"  "+chalk.blue(x.href)+"  "+chalk.cyan(x.text.substr(0,40))+"  "+x.status)
-//     return console.log(validateLinks.join("\n "))
-//   }
-//   if(options.stats){
-//     return console.log(chalk.magenta("Total Links: "+ res.total)+"\n"+chalk.yellow("Unique Links: "+res.unique))
-//   }else{
-//     if(res.length === 0){
-//       return console.log(chalk.red("No se encontraron links"))
-//     } 
-//     const resLinks = res.map(x=>x.file+"  "+chalk.blue(x.href)+"  "+chalk.cyan(x.text.substr(0,40)))
-//     return console.log(resLinks.join("\n "))
-//   }
-// }).catch(err=>{
-//   console.log(chalk.red(err.message))
-// });
+mdLinks.mdLinks(path,options).then(res => {
+  if(options.validate && options.stats){
+    return console.log(chalk.bgBlue("Total Links: " + res.total) + "\n" + chalk.bgGreen("Ok Links: " + res.ok) + "\n" + chalk.bgRed("Broken Links: " + res.broken))
+  }
+  if(options.validate){
+    if(res.length === 0){
+      return console.log(chalk.red("No se encontraron links"))
+    }
+    let validateLinks = res.map(element => chalk.bgBlack(element.file) +"  " + chalk.yellowBright(element.href) + "  " + chalk.cyanBright(element.text.substr(0,50)) + "  " + chalk.bgMagenta(element.status) + " " + chalk.bgGreen(element.statusCode))
+    return console.log(validateLinks.join("\n "))
+  }
+  if(options.stats){
+    return console.log(chalk.bgBlue("Total Links: "+ res.total) + "\n" + chalk.bgMagenta("Unique Links: "+res.unique))
+  }else{
+    if(res.length === 0){
+      return console.log(chalk.red("No se encontraron links"))
+    } 
+    const resLinks = res.map(element => element.file + "  " + chalk.yellowBright(element.href) + "  " + chalk.cyanBright(element.text.substr(0,50)))
+    return console.log(resLinks.join("\n "))
+  }
+}).catch(err=>{
+  console.log(chalk.red(err.message))
+});
--- src/md-links.js
+++ src/md-links.js
@@ -1,84 +1,11 @@
-#!/usr/bin/env node
-'use strict'; 
-const process = require('process');
-const fetch = require('node-fetch');
-const fetchUrl = fetch.fetchUrl;
-const pathNode = require('path');
-const FileHound = require('filehound');
-const commander = require('commander');
 const fs = require('fs');
 const marked = require('marked');
+const FileHound = require('filehound');
+const fetch = require('node-fetch');
 
-
-  let userPath = process.argv[2];
-  // path.resolve para convertir en absoluta  
-  userPath = pathNode.resolve(userPath);
-  // path.normalize para normalizar el path en caso se que hayan errores de semántica
-  userPath = pathNode.normalize(userPath);
-  
-
-// Lee los archivos y extrae links con info: link, texto 
-const searchLinks = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      let links = [];
-      const renderer = new marked.Renderer();
-      renderer.link = function (href, title, text) {
-        links.push({
-          // Url encontrada.
-          href: href,
-          // Texto que aparece acompañando al link.
-          text: text,
-          // Ruta del archivo donde se encontró el link.
-          file: path
-        })
-      }
-      marked(data, {
-        renderer: renderer
-      })
-      resolve(links)
-      validateLinks(links);
-      //console.log(statsLinks(links));
-    });
-  })
-};
-
-const validateLinks = (links) => {
-  return new Promise((resolve, reject) => {
-     
-    let fetchLinks = links.map(element => {  
-      
-      return fetch(element.href).then(res =>{
-          element.statusCode = res.status;
-          element.status = res.statusText;
-        }).catch((err)=>{
-          element.status = err.code
-        }) 
-    })
-    
-    Promise.all(fetchLinks).then(res => {
-      resolve(links)
-      console.log(links);
-    })
-  })
-}
-
-//stats de cada link 
-const statsLinks = (links, path) => {
-  let href = links.map(element => element.href);
-  const uniqueLinks = new Set(href);
-  return {
-    path: userPath,
-    total: links.length,
-    unique: uniqueLinks.size
-  }
-}
 
 //FILEHOUND para encontrar archivos MD dentro un directorio
-const findMdFiles = (path => {
+const findMdFiles = (path =>{
   return new Promise((resolve,reject) => {
     FileHound.create()
     .paths(path)
@@ -86,14 +13,52 @@ const findMdFiles = (path => {
     .find()
     .then(files => {
       if(files.length != 0){
-      resolve(files)
-      // findInDirectory(files);
-      }
-      else {(console.log("No se encontraron archivos .md dentro de " + path))}
+      resolve(files)}
+      reject(new Error("No se encontraron archivos .md dentro de " + path))
     })
     .catch(err => {
-    reject(new Error("Ruta no es válida"))
+      reject(new Error("Ruta no es válida"))
     })
+  })
+})
+
+
+//leer archivo.md
+const readMd = (path => {
+  return new Promise((resolve,reject)=>{
+    fs.readFile( path,'utf8', (err, data) => {
+      if (err){
+        reject(new Error("No se encontro el archivo " + path))
+      }
+      resolve(data)
+    })
+  })
+})
+
+//obtener links de un archivo .md
+const searchLinks = (path => {
+  return new Promise((resolve, reject) => {
+    readMd(path).then(res => {
+      let links = [];
+      const renderer = new marked.Renderer();
+      renderer.link = function(href,title,text){
+
+        if(!href.startsWith("mailto:")){
+          links.push({
+            // Url encontrada.
+            href:href,
+            // Texto que aparece acompañando al link.
+            text:text,
+            // Ruta del archivo donde se encontró el link.
+            file:path})
+        }
+      } 
+        marked(res,{renderer:renderer}); 
+        resolve(links)
+    })
+    .catch(err => {
+      reject(err)
+      })
   })
 })
 
@@ -103,37 +68,41 @@ const findInDirectory = (files) =>{
     let count = 0;
     let allLinks = []
     files.forEach(element => {
-      searchLinks(element).then(singleLink => {
+      searchLinks(element).then(singleLink =>{
         count++
         allLinks = allLinks.concat(singleLink)
         if(count == files.length){
           resolve(allLinks)
         }
-      }).catch(err => {
+      }).catch(err=>{
           reject(err)
         })
     })
   })
 }
 
-//comprobar si userPath es archivo o directorio
+// true si el archivo es .md --> necesario comprobar
+const isMd = (path =>{
+  if(path.slice(-3)== ".md"){
+    return true;
+  }
+  return false;
+})
+
+//comprobar si path es archivo o directorio
 const fileOrDirectory = (path) => {
-  let conditionFile = fs.statSync(path)
-  //PONER EL ERROR CUANDO LA RUTA NO EXISTE LA RUTA
-  //si es archivo
-  if (conditionFile.isFile() === true){
-    searchLinks(path);
+   //checkea si es archivo MD
+  if(isMd(path)){
+    return searchLinks(path)
   }
    //si es directorio
-  // if (conditionFile.isFile() === false){
-  else{
+  else {
       return new Promise((resolve, reject) => { 
         findMdFiles(path)
         .then(files => {
           findInDirectory(files)
           .then(links => {
             resolve(links)
-            //console.log(links);
           })
         }).catch(err =>{
           reject(new Error(err.message))
@@ -141,47 +110,90 @@ const fileOrDirectory = (path) => {
       })
     }
   }
-// }
-fileOrDirectory(userPath);
+  
+  //[option --validate --stats]
+const statsAndValidateLinks = (path) =>{
+  return new Promise((resolve,reject)=>{
+    validateLinks(path).then(links=>{
+      const statusLinks = links.map(element => element.status)
+      let okLinks = statusLinks.toString().match(/200/g)
+      const totalLinks = links.length
+      let brokenLinks = 0
 
-
-
-//  //comandos (COMMANDER)----------------------------------------------
-//  const program = new commander.Command();
-//  program
-//   .option('-v, --validate', 'ruta del archivo, link, texto, y validacion de link')
-//   .option('-s, --stats', 'contador de links, contador de link unicos')
- 
-// program.parse(process.argv);
-
-// if (!program.validate && !program.stats) console.log(`Path: ${filePath}`);
-// if (program.validate !== undefined) console.log(`Path: ${fileName}, Link: ${fileName}, Texto: ${fileName}, Link Status: ${fileName}, HTTP Status: ${fileName}`);
-// if (program.stats !== undefined) console.log(`Total: ${totalLinks}, Unique: ${totalLinks}`);
-// if (program.validate !== undefined && program.stats !== undefined) console.log(`Total: ${totalLinks}, Unique: ${totalLinks}, Broken: ${brokenLinks}`);
-
-
-// let options = {
-//      stats: false,
-//      validate: false
-// } 
-
-// let firstOption = process.argv[3];
-// let secondOption = process.argv[4];
-
-// if(firstOption ==="--validate" && secondOption === "--stats"||firstOption==="--stats" && secondOption === "--validate"||firstOption ==="--v" && secondOption === "--s"||firstOption==="--s" && secondOption === "--v"){
-//      options.validate = true;
-//      options.stats = true;
-// }else if(firstOption==="--stats"|| firstOption === "--s"){
-//      options.stats = true;
-//      options.validate = false;
-// }else if(firstOption==="--validate"|| firstOption === "--v"){
-//      options.validate = true;
-//      options.stats = false;
-// };
+      if(okLinks != null){
+        okLinks = okLinks.length
+      }else{
+        okLinks =  0
+      }
+      
+      brokenLinks = totalLinks - okLinks
+      resolve({
+        total: totalLinks,
+        ok: okLinks,
+        broken: brokenLinks
+      })
+    }).catch(err=>{
+      reject(err)
+    })
+  })
+}
+// [option --validate]
+const validateLinks = (path) => {
+  return new Promise((resolve, reject) => {
+    fileOrDirectory(path)
+    .then(links => { 
+      let fetchLinks = links.map(element => {  
+        return fetch(element.href).then(res => {
+          element.statusCode = res.status;
+          element.status = res.statusText;
+        })
+        .catch((err) => {
+          element.status = err.code
+        }) 
+    })
+      Promise.all(fetchLinks).then(res => {
+          resolve(links)
+      })
+    })
+    .catch(err=>{
+      reject(err)
+    })
+  })
+}
+// [option --stats]
+const statsLinks = (path) =>{
+return new Promise((resolve, reject) => { 
+  fileOrDirectory(path)
+  .then(links =>{
+    const uniqueLinks = new Set(links.map(element => element.href))
+    resolve({total:links.length,
+      unique : uniqueLinks.size})
+    })
+  .catch(err=>{
+    reject(err)
+  })
+  })
+}
+//
+const mdLinks = (path, options) =>{
+  if(!path || !options){
+    return new Promise((resolve,reject)=>{
+      reject(new Error ("Faltan argumentos"))
+    })
+  }
+  if(options.validate && options.stats){
+    return statsAndValidateLinks(path)
+  }  
+  if(options.validate){
+    return validateLinks(path)
+  }
+  if(options.stats){
+    return statsLinks(path)
+  }
+  else{
+    return fileOrDirectory(path)}
+  }
 
 module.exports = {
-  fileOrDirectory,
-  findMdFiles,
-  searchLinks,
-  findInDirectory
+  mdLinks 
 }
